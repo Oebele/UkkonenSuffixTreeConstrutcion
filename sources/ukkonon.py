850f5199b715500c6be1a4f6ee407578b686b653
Oebele <ebele.gmail.com>
2018-11-05 11:25:09 +0100

    First commit

---
+++ sources/ukkonon.py
@@ -0,0 +1,171 @@
+from dataclasses import dataclass, field
+from typing import Any, Dict, Tuple
+from math import inf
+
+
+class STree:
+    def __init__(self, string, alphabet, hashtag='#'):
+        self.t = f' {string}{hashtag}'  # T = t1 t2 ... #
+        self.bottom = None
+        self.root = None
+        self.alphabet = alphabet
+        self.hashtag = hashtag
+
+    def suffix_tree(self):
+        """Construction of STree(T) for string T = t1 t2 . . . # in alphabet
+        Σ = {t −1 , ... , t −m }; # is the end marker not appearing elsewhere
+        in T.
+
+        Arguments:
+            s {string} -- T = t1 t2 . . .
+            alphabet {alphabet} -- Σ = {t −1 , ... , t −m }
+        return bottom {Node} -- ⊥
+        """
+        # self.t is T = t1 t2 ... #
+        # create states root and ⊥;
+        self.bottom, self.root = STree.Node(), STree.Node()
+        for j in range(len(self.alphabet)):  # for j ← 1, ... , m do
+            # create transition g0(⊥, (−j, −j)) = root;
+            self.bottom.g0(-j - 1, -j - 1, self.root)
+        self.root.f = self.bottom  # create suffix link f0(root) = ⊥;
+        s, k, i = self.root, 1, 0  # s ← root; k ← 1; i ← 0;
+        while self.t[i + 1] != self.hashtag:  # while t i+1 != # do
+            i += 1  # i ← i + 1;
+            s, k = self.update(s, k, i)  # (s, k) ← update(s, (k, i));
+            s, k = self.canonize(s, k, i)  # (s, k) ← canonize(s, (k, i)).
+
+    def update(self, s, k, i):  # procedure update(s, (k, i)):
+        # (s, (k, i − 1)) is the canonical reference pair for the active point;
+        oldr = self.root  # oldr ← root;
+        # (end–point, r) ← test–and–split(s, (k, i − 1), t i );
+        end_point, r = self.test_and_split(s, k, i - 1, self.t[i])
+        while not end_point:  # while not(end–point) do
+            # create new transition g0(r, (i, ∞)) = r0 where r0 is a new state;
+            r.g0(i, inf, None)
+            # if oldr != root then create new suffix link f0(oldr) = r;
+            if oldr is not self.root:
+                oldr.f = r
+            oldr = r  # oldr ← r;
+            # (s, k) ← canonize(f0(s), (k, i − 1));
+            s, k = self.canonize(s.f, k, i - 1)
+            # (end–point, r) ← test–and–split(s, (k, i − 1), ti);
+            end_point, r = self.test_and_split(s, k, i - 1, self.t[i])
+        # if oldr != root then create new suffix link f0 (oldr) = s;
+        if oldr != self.root:
+            oldr.f = s
+        # return (s, k).
+        return s, k
+
+    def canonize(self, s, k, p):  # procedure canonize(s, (k, p)):
+        # if p < k then return (s, k)
+        if p < k:
+            return s, k
+        # else
+        # find the tk–transition g0(s, (k0, p0)) = s0 from s;
+        k0, p0, s0 = s.find_tk_transaction(self, k)
+        while p0 - k0 <= p - k:  # while p0 − k0 ≤ p − k do
+            k = k + p0 - k0 + 1  # k ← k + p0 − k0 + 1;
+            s = s0  # s ← s0 ;
+            # if k ≤ p then find the tk–transition g0(s, (k0, p0)) = s0 from s;
+            if k <= p:
+                k0, p0, s0 = s.find_tk_transaction(self, k)
+        # return (s, k).
+        return s, k
+
+    def test_and_split(self, s, k, p, t):
+        # procedure test–and–split(s, (k, p), t):
+        if k <= p:  # if k ≤ p then
+            # let g0(s, (k0, p0 )) = s0 be the tk–transition from s;
+            k0, p0, s0 = s.find_tk_transaction(self, k)
+            # if t = tk0+p−k+1 then return(true, s)
+            if t == self.t[k0 + p - k + 1]:
+                return True, s
+            # else
+            # replace the tk–transition above by transitions
+            # g0(s, (k0, k0 + p − k)) = r and g0(r, (k0 + p − k + 1, p0)) = s0
+            r = STree.Node()
+            s.g0(k0, k0 + p - k, r)
+            r.g0(k0 + p - k + 1, p0, s0)
+            # where r is a new state;
+            # return(false, r)
+            return False, r
+        else:  # else
+            # if there is no t–transition from s then return(false, s)
+            if s.no_t_transaction(self, t):
+                return False, s
+            else:  # else return(true, s).
+                return True, s
+
+    def __str__(self):
+        return '\nroot' + self.root._to_str(self)
+
+    @dataclass
+    class Node:
+        f: Any = None
+        g: Dict[int, Tuple[int, Any]] = field(default_factory=dict)
+
+        def g0(self, k, p, s):
+            self.g[k] = p, s
+
+        def find_tk_transaction(self, stree, k):
+            for k0, (p0, s0) in self.g.items():
+                if k0 < 0 and stree.alphabet[-k0 - 1] == stree.t[k]:
+                    return k0, p0, s0
+                if k0 > 0 and stree.t[k0] == stree.t[k]:
+                    return k0, p0, s0
+            raise Exception(f'No tk transaction in {self}')
+
+        def no_t_transaction(self, stree, t):
+            for k0, _ in self.g.items():
+                if k0 < 0 and stree.alphabet[-k0 - 1] == t:
+                    return False
+                if k0 > 0 and stree.t[k0] == t:
+                    return False
+            return True
+
+        def __eq__(self, other):
+            if self is other:
+                return True
+            return self._equal(other, set())
+
+        def __hash__(self):
+            return id(self)
+
+        def _equal(self, other, processed):
+            processed.add(self)
+            if not self.f:
+                if other.f:
+                    return False
+            elif not other.f:
+                return False
+            elif (self.f not in processed and
+                    not self.f._equal(other.f, processed)):
+                return False
+
+            if len(self.g) != len(other.g):
+                return False
+
+            for k, (p, s) in self.g.items():
+                if k not in other.g:
+                    return False
+                p0, s0 = other.g[k]
+                if p0 != p:
+                    return False
+                if s and s not in processed:
+                    processed.add(s)
+                    if not s._equal(s0, processed):
+                        return False
+            return True
+
+        def _to_str(self, stree, tabs=4):
+            result, tab = '', ''
+            for k, (p, s) in self.g.items():
+                if k < 0:
+                    result += tab * tabs + f'letter = Node({k}, {p} = {stree.alphabet[-k - 1]}) '  # nopep8
+                elif s:
+                    text = f'-------Node({k}, {p} = {stree.t[k: p + 1]})'
+                    result += tab * tabs + text + s._to_str(stree, tabs + 21)  # nopep8
+                else:
+                    result += tab * tabs + f'-------Node({k}, {p} = {stree.t[k:]})\n'  # nopep8
+                tab = ' '
+            return result
---
+++ tests/test_ukkonon.py
@@ -0,0 +1,365 @@
+from pytest import raises
+from sources.ukkonon import STree
+from math import inf
+
+Node = STree.Node
+
+# for coverage :
+# python3.7 -m pytest --cov=sources/ --cov-report=html  tests/test_ukkonon.py
+
+
+def test_ukkonen_empty_string():
+    s = ''
+    bottom, root, alphabet = create_bottom_and_node()
+    expected = bottom
+
+    st = STree(s, alphabet)
+    st.suffix_tree()
+    result = st.bottom
+    assert result == expected
+
+
+def test_ukkonen_string_c():
+    s = 'c'
+    alphabet, _, expected = create_expected_for_string_c()
+
+    st = STree(s, alphabet)
+    st.suffix_tree()
+    result = st.bottom
+
+    assert result == expected
+
+
+def test_ukkonen_string_ca():
+    s, alphabet, expected = create_expected_for_string_ca()
+
+    st = STree(s, alphabet)
+    st.suffix_tree()
+    result = st.bottom
+    assert result == expected
+
+
+def test_ukkonen_string_cac():
+    s = 'cac'
+    _, alphabet, expected = create_expected_for_string_ca()
+
+    st = STree(s, alphabet)
+    st.suffix_tree()
+    result = st.bottom
+    assert result == expected
+
+
+def test_ukkonen_string_caca():
+    s = 'caca'
+    _, alphabet, expected = create_expected_for_string_ca()
+
+    st = STree(s, alphabet)
+    st.suffix_tree()
+    result = st.bottom
+    assert result == expected
+
+
+def test_ukkonen_string_cacao():
+    s = 'cacao'
+    _, alphabet, expected = create_expected_for_string_ca()
+    bottom = expected
+    root = bottom.g[-1][1]
+    root.g0(5, inf, None)
+    cacao = Node()
+
+    acao = Node()
+    cacao.f = acao
+    acao.f = root
+    root.g0(1, 2, cacao)
+    root.g0(2, 2, acao)
+    root.g0(5, inf, None)
+    cacao.g0(3, inf, None)
+    cacao.g0(5, inf, None)
+    acao.g0(3, inf, None)
+    acao.g0(5, inf, None)
+
+    st = STree(s, alphabet)
+    st.suffix_tree()
+    result = st.bottom
+
+    assert result == expected
+
+
+def test_string_banana_wikipedia():
+    s = 'banana$'
+    expected, root, alphabet = create_bottom_and_node(sorted(set(s)))
+    root_a = Node()
+    root_na = Node()
+
+    root_a.f = root
+    root_na.f = root_a
+
+    a_na = Node()
+    a_na.f = root_na
+
+    root.g0(2, 2, root_a)  # a
+    root.g0(1, inf, None)  # banana$
+    root.g0(3, 4, root_na)   # na
+    root.g0(7, inf, None)  # $
+
+    root_a.g0(7, inf, None)  # $
+    root_a.g0(3, 4, a_na)  # na
+
+    root_na.g0(7, inf, None)  # $
+    root_na.g0(5, inf, None)  # na$
+
+    a_na.g0(7, inf, None)  # $
+    a_na.g0(5, inf, None)  # na$
+
+    st = STree(s, alphabet)
+    st.suffix_tree()
+    result = st.bottom
+
+    assert result == expected
+
+
+def test_string_mississippi():
+    # added for better coverage criteia
+    s = 'mississippi$'
+    expected, root, alphabet = create_bottom_and_node(sorted(set(s)))
+    root_i = Node()
+    root_s = Node()
+    root_p = Node()
+
+    root_i.f = root
+    root_s.f = root
+    root_p.f = root
+
+    root.g0(1, inf, None)  # mississiooi$
+    root.g0(2, 2, root_i)  # i
+    root.g0(3, 3, root_s)  # s
+    root.g0(9, 9, root_p)  # p
+    root.g0(12, inf, None)  # $
+
+    i_ssi = Node()
+    root_i.g0(3, 5, i_ssi)  # ssi
+    root_i.g0(9, inf, None)  # ppi$
+    root_i.g0(12, inf, None)  # $
+
+    i_ssi.g0(6, inf, None)  # ssippi$
+    i_ssi.g0(9, inf, None)  # ppi$
+
+    s_si = Node()
+    s_i = Node()
+
+    i_ssi.f = s_si
+    s_si.f = s_i
+    s_i.f = root_i
+
+    s_si.g0(6, inf, None)  # ssippi$
+    s_si.g0(9, inf, None)  # ppi$
+
+    s_i.g0(6, inf, None)  # ssippi$
+    s_i.g0(9, inf, None)  # ppi$
+
+    root_p.g0(11, inf, None)  # i$
+    root_p.g0(10, inf, None)  # pi$
+
+    st = STree(s, alphabet)
+    st.suffix_tree()
+    result = st.bottom
+    print(st)
+    assert expected == expected
+
+
+def create_expected_for_string_ca():
+    s = 'ca'
+    alphabet, root, expected = create_expected_for_string_c()
+    root.g0(2, inf, None)
+    return s, alphabet, expected
+
+
+def create_expected_for_string_c():
+    bottom, root, alphabet = create_bottom_and_node()
+    root.g0(1, inf, None)
+    expected = bottom
+    return alphabet, root, expected
+
+
+# --------tests for equal function------------------------------------
+def test_equal_function_empty_node():
+    bottom = Node()
+    root = Node()
+    assert bottom == root
+
+
+def test_1():
+    bottom, root, alphabet = create_bottom_and_node()
+    assert bottom != root
+
+
+def test_2():
+    bottom, root, alphabet = create_bottom_and_node()
+    bottom1, root1, _ = create_bottom_and_node()
+    assert bottom == bottom1
+
+
+def test_3():
+    bottom, root, alphabet = create_bottom_and_node()
+    g = Node()
+    root.g0(1, 1, g)
+    g.f = root
+    bottom1, root1, alphabet = create_bottom_and_node()
+    assert bottom != bottom1
+
+
+def test_4():
+    bottom, root, alphabet = create_bottom_and_node()
+    g = Node()
+    root.g0(1, 1, g)
+
+    bottom1, root1, alphabet = create_bottom_and_node()
+    g1 = Node()
+    root1.g0(1, 1, g1)
+    assert bottom == bottom1
+
+
+def test_5():
+    bottom, root, alphabet = create_bottom_and_node()
+    g = Node()
+    root.g0(1, 1, g)
+    g.g0(2, 1, None)
+
+    bottom1, root1, alphabet = create_bottom_and_node()
+    g1 = Node()
+    root1.g0(1, 1, g1)
+    assert bottom != bottom1
+
+
+def test_6():
+    bottom, root, alphabet = create_bottom_and_node()
+    g = Node()
+    root.g0(1, 1, g)
+    g.g0(2, 1, None)
+
+    bottom1, root1, alphabet = create_bottom_and_node()
+    g1 = Node()
+    root1.g0(1, 1, g1)
+    g1.g0(2, 1, None)
+    assert bottom == bottom1
+
+
+def test_7():
+    bottom, root, alphabet = create_bottom_and_node()
+    g = Node()
+    root.g0(1, 1, g)
+    g.g0(2, 1, None)
+
+    bottom1, root1, alphabet = create_bottom_and_node()
+    g1 = Node()
+    root1.g0(1, 1, g1)
+    g1.g0(2, 1, None)
+    assert bottom1 == bottom
+
+
+def test_8():
+    bottom, root, alphabet = create_bottom_and_node()
+    g = Node()
+    root.g0(1, 1, g)
+    g.g0(2, 1, None)
+
+    bottom1, root1, alphabet = create_bottom_and_node()
+    g1 = Node()
+    root1.g0(1, 1, g1)
+    g1.g0(2, inf, None)
+    assert bottom != bottom1
+
+
+def test_9():
+    bottom, root, alphabet = create_bottom_and_node()
+    g = Node()
+    root.g0(1, 1, g)
+    g.g0(2, 1, None)
+
+    bottom1, root1, alphabet = create_bottom_and_node()
+    g1 = Node()
+    root1.g0(1, 1, g1)
+    g1.g0(2, inf, None)
+    assert bottom != bottom1
+
+
+def test_10():
+    bottom, root, alphabet = create_bottom_and_node()
+    g = Node()
+    root.g0(1, 1, g)
+    g.g0(2, 2, None)
+    g.g0(3, inf, None)
+    g.f = g
+
+    bottom1, root1, alphabet = create_bottom_and_node()
+    g1 = Node()
+    root1.g0(1, 1, g1)
+    g1.g0(2, 2, None)
+    g1.g0(3, inf, None)
+    assert bottom != bottom1
+
+
+def test_11():
+    bottom, root, alphabet = create_bottom_and_node()
+    g = Node()
+    root.g0(1, 1, g)
+    g.g0(2, 1, None)
+    g.g0(2, inf, None)
+    g.f = g
+
+    bottom1, root1, alphabet = create_bottom_and_node()
+    g1 = Node()
+    root1.g0(1, 1, g1)
+    g1.g0(2, 1, None)
+    g1.g0(2, inf, None)
+    assert bottom1 != bottom
+
+
+def test_12():
+    bottom, root, alphabet = create_bottom_and_node()
+    g = Node()
+    root.g0(1, 1, g)
+    g.g0(2, 2, None)
+    g.g0(4, inf, None)
+    g.f = root
+
+    bottom1, root1, alphabet = create_bottom_and_node()
+    g1 = Node()
+    root1.g0(1, 1, g1)
+    g1.g0(2, 2, None)
+    g1.g0(4, inf, None)
+    g1.f = root
+    assert bottom == bottom1
+
+
+def test_13():
+    bottom, root, alphabet = create_bottom_and_node()
+    g = Node()
+    root.g0(1, 1, g)
+    g.g0(2, 1, None)
+
+    bottom1, root1, alphabet = create_bottom_and_node()
+    g1 = Node()
+    root1.g0(1, 1, g1)
+    g1.g0(3, inf, None)
+    assert bottom != bottom1
+
+
+def test_14():
+    bottom, root, alphabet = create_bottom_and_node()
+    g = Node()
+    root.g0(1, 1, g)
+    g.g0(2, 1, None)
+    STree.t = alphabet
+    STree.alphabet = alphabet
+    with raises(Exception):
+        g.find_tk_transaction(STree, 1)
+
+
+def create_bottom_and_node(alphabet='aco'):
+    bottom = Node()
+    root = Node()
+    for j in range(len(alphabet)):
+        bottom.g0(-j - 1, -j - 1, root)
+    root.f = bottom
+    return bottom, root, alphabet
